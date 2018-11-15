a62a92128e759a17f7b9acb67151e170f2060fb7
Dmitriy Ashmarin <ashmarindmitrii@gmail.com>
2018-11-15 18:16:18 +0300

    Подключен slick-slider в rating. Настроены стили

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
-  $('.rating__slider').slick({
-    autoplay: true,
+  $('.rating__backgrounds').slick({
     infinite: true,
-    speed: 1000,
+    dots: true,
+    swipe: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $(`.prev`),
+    nextArrow: $(`.next`),
+    appendDots: $(`.interactive__bullets`),
+    dotsClass: `js-rating-bullets`,
+    asNavFor: `.interactive__previews`
+  });
+  $(`.interactive__previews`).slick({
+    arrows: false,
+    dots: false,
+    infinite: true,
+    asNavFor: `.rating__backgrounds`,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true
   });
 });
